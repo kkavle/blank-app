ca2294853228dd0e0c2266f90bcf2b22690179d3
Karishma Kavle <kkavle@users.noreply.github.com>
2024-12-09 10:28:19 -0800

    Update streamlit_app.py foortie

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,7 @@ df = conn.query("SELECT * FROM mytable;", ttl="10m")
 
 for row in df.itertuples():
     st.write(f"{row.NAME} has a :{row.PET}:")
+    
+import requests
+smoothiefroot_response = requests.get("https://smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response.json())
